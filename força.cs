3693fdfa14bb7796198d092b845d6435a736fa0e
Leonardo Celso de Souza <leonardo_souzac@hotmail.com>
2020-03-05 16:40:45 -0300

    Add files via upload

---
+++ força.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class força : MonoBehaviour
+{
+    private Rigidbody2D bola;
+    private float force = 0f;
+    private rotação rot;
+    public Image seta2Img;
+    void Start()
+    {
+        bola = GetComponent<Rigidbody2D>();
+        rot = GetComponent<rotação>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AplicaForca();
+        ControlaForca();
+    }
+
+    void AplicaForca()
+    {
+        float x =  force * Mathf.Cos(rot.zRotate * Mathf.Deg2Rad);
+        float y =  force * Mathf.Sin(rot.zRotate * Mathf.Deg2Rad);
+
+        if (rot.liberaTiro == true)
+        {
+            bola.AddForce(new Vector2(x, y));
+            rot.liberaTiro = false;
+        }
+    }
+
+    void ControlaForca ()
+    {
+        if (rot.liberaRot == true)
+        {
+            float moveX = Input.GetAxis("Mouse X");
+
+            if (moveX < 0)
+            {
+                seta2Img.fillAmount += 1 * Time.deltaTime;
+                force = seta2Img.fillAmount * 1000;
+            }
+            if (moveX > 0)
+            {
+                seta2Img.fillAmount -= 1 * Time.deltaTime;
+                force = seta2Img.fillAmount * 1000;
+            }
+        }
+    }
+}
---
+++ rotação.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rotação : MonoBehaviour
+{
+    //Posição seta
+    [SerializeField] private Transform posStart;
+    //Seta
+    [SerializeField] private Image setaImg;
+    //Angulo
+    public float zRotate;
+    public bool liberaRot = false;
+    public bool liberaTiro = false;
+
+    void Start()
+    {
+        PosicionaSeta ();
+        PosicionaBola ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotacaoSeta ();
+        LimitaRotacao ();
+        InputDeRotacao ();
+    }
+    void FixedUpdate ()
+    {
+        
+    }
+    void PosicionaSeta ()
+    {
+        setaImg.rectTransform.position = posStart.position;
+
+
+    }
+
+    void PosicionaBola ()
+    {
+        this.gameObject.transform.position = posStart.position;
+    }
+
+    void RotacaoSeta () 
+    {
+        setaImg.rectTransform.eulerAngles = new Vector3 (0,0,zRotate);
+
+    }
+
+    void InputDeRotacao ()
+    {
+        /*if (Input.GetKey(KeyCode.UpArrow))
+        {
+            zRotate += 2.5f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            zRotate -= 2.5f;
+        }*/
+
+        if(liberaRot == true)
+        {
+            
+            float moveY = Input.GetAxis("Mouse Y");
+
+            if (zRotate < 90)
+            {
+                if (moveY > 0)
+                {
+                    zRotate += 2.5f;
+                }
+            }
+            if (zRotate > 0)
+                {
+                    if(moveY < 0)
+                    {
+                    zRotate -= 2.5f;
+                    }
+                }
+            
+           /* if (moveY > 0)
+            {
+                zRotate += 2.5f;
+            }
+            if (moveY < 0)
+            {
+                zRotate -= 2.5f;
+            }*/
+        }
+    }
+
+    void LimitaRotacao ()
+    {
+        if (zRotate >= 90)
+        {
+            zRotate = 90;
+        }
+
+        if (zRotate <= 0)
+        {
+            zRotate = 0;
+        }
+    } 
+
+    void OnMouseDown ()
+    {
+        liberaRot = true;
+    
+    }
+    
+    void OnMouseUp ()
+    {
+        liberaRot = false;
+        liberaTiro = true;
+    }
+}
